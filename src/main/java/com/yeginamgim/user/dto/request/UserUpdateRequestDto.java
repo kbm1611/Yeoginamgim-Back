34940b7874117ca0da73bbcc6c750d13597b1eca
bm1611 <bm1611@naver.com>
2026-05-28 13:18:17 +0900

    회원 정보수정 요청에 대한 Dto 생성

---
+++ src/main/java/com/yeginamgim/user/dto/request/UserUpdateRequestDto.java
@@ -0,0 +1,14 @@
+package com.yeginamgim.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+// 회원 정보수정 요청에 대한 Dto
+public class UserUpdateRequestDto {
+}
