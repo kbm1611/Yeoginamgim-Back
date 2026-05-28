9804e7e2f2f499cdbcc66dbfec867956275ea8f0
bm1611 <bm1611@naver.com>
2026-05-28 16:55:00 +0900

    로그인 요청, 응답 객체 Dto 생성

---
+++ src/main/java/com/yeginamgim/auth/dto/request/LoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.yeginamgim.auth.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
