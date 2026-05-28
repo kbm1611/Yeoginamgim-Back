2b29504561f12618641917c14efbb14190b462bf
bm1611 <bm1611@naver.com>
2026-05-28 16:35:12 +0900

    UserController(일반 회원가입, 회원정보 조회, 회원정보 수정)

--- src/main/java/com/yeginamgim/user/controller/UserController.java
+++ src/main/java/com/yeginamgim/user/controller/UserController.java
@@ -1,17 +1,66 @@
 package com.yeginamgim.user.controller;
 
+import com.yeginamgim.auth.jwt.JWTService;
+import com.yeginamgim.user.dto.request.UserSignupRequestDto;
+import com.yeginamgim.user.dto.request.UserUpdateRequestDto;
+import com.yeginamgim.user.dto.response.UserInfoResponseDto;
 import com.yeginamgim.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 // [1] 회원 정보 관리
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
     private final UserService userSvc;
-    // (1) 회원 정보 조회, 로그인한 사용자 정보 조회
-    // (2) 닉네임 수정
-    // (3) 프로필 이미지 조회
-    // (4) 사용자 권한 관리
-    // (5) 사용자가 남긴 포스트잇과 연결
+    private final JWTService jwtSvc;
+    // (1) 일반 회원가입
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@ModelAttribute UserSignupRequestDto userReqDto){
+        return ResponseEntity.ok(userSvc.signup(userReqDto));
+    }
+    // (2) 회원 정보 조회, 로그인한 사용자 정보 조회
+    @GetMapping("/myinfo")
+    public ResponseEntity<?> getMyInfo(@RequestHeader(value = "Authorization") String token){
+        if(token == null || !token.startsWith("Bearer")){
+            return ResponseEntity.ok( false );
+        }
+        token = token.replace("Bearer ", "");
+        String email = jwtSvc.getClaim(token);
+        if(email == null){
+            return ResponseEntity.ok( false );
+        }
+        UserInfoResponseDto result = userSvc.getMyInfo(email);
+        if( result == null ){
+            return ResponseEntity.status(404).body("회원 정보가 없습니다.");
+        }
+        return ResponseEntity.ok(result);
+    }
+    // (3) 회원정보 수정
+    @PatchMapping("/update")
+    public ResponseEntity<?> updateUserInfo(
+            @RequestHeader(value = "Authorization") String token,
+            @ModelAttribute UserUpdateRequestDto userUpdDto){
+
+        if (token == null || !token.startsWith("Bearer ")) {
+            return ResponseEntity.ok(false);
+        }
+
+        token = token.replace("Bearer ", "");
+        String email = jwtSvc.getClaim(token);
+
+        if (email == null) {
+            return ResponseEntity.ok(false);
+        }
+
+        UserInfoResponseDto result = userSvc.updateUserInfo(email, userUpdDto);
+
+        if (result == null) {
+            return ResponseEntity.status(404).body("회원 정보를 찾을 수 없습니다.");
+        }
+
+        return ResponseEntity.ok(result);
+    }
 }
