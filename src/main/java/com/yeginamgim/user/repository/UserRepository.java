5f5605533f762cf0cbe92b036dd89d35de5e888e
bm1611 <bm1611@naver.com>
2026-05-28 16:59:44 +0900

    findByProviderAndProviderId 생성

--- src/main/java/com/yeginamgim/user/repository/UserRepository.java
+++ src/main/java/com/yeginamgim/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.yeginamgim.user.repository;
 
 import com.yeginamgim.user.entity.UserEntity;
+import com.yeginamgim.user.enums.LoginProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByProviderAndProviderId(LoginProvider provider, String providerId);
     Optional<UserEntity> findByEmail(String email);
 }
