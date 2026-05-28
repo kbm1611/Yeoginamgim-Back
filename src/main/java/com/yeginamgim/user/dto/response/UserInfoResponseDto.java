5856e633ea606faef3cf8e015b73a808a442368f
bm1611 <bm1611@naver.com>
2026-05-28 13:17:46 +0900

    회원 정보 응답에 대한 Dto 생성

---
+++ src/main/java/com/yeginamgim/user/dto/response/UserInfoResponseDto.java
@@ -0,0 +1,14 @@
+package com.yeginamgim.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+// 회원 정보 응답에 대한 Dto
+public class UserInfoResponseDto {
+}
