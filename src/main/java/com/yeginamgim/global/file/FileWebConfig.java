270e94e9033b640b03811ac9974add505cacf7ce
bm1611 <bm1611@naver.com>
2026-05-29 16:07:25 +0900

    파일 경로 설정 파일 생성

---
+++ src/main/java/com/yeginamgim/global/file/FileWebConfig.java
@@ -0,0 +1,19 @@
+package com.yeginamgim.global.file;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.file.Path;
+
+@Configuration
+public class FileWebConfig implements WebMvcConfigurer {
+
+    private final Path uploadRoot = Path.of(System.getProperty("user.dir"), "uploads");
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations(uploadRoot.toUri().toString());
+    }
+}
