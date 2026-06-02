aced33c191b00176059746210abc84434da7f32f
bm1611 <bm1611@naver.com>
2026-06-02 12:46:35 +0900

    예외 던지기

--- src/main/java/com/yeginamgim/user/service/UserService.java
+++ src/main/java/com/yeginamgim/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.yeginamgim.user.service;
 
 import com.yeginamgim.global.exception.DuplicateMemberException;
+import com.yeginamgim.global.exception.UserNotFoundException;
 import com.yeginamgim.global.file.FileService;
 import com.yeginamgim.user.dto.request.UserSignupRequestDto;
 import com.yeginamgim.user.dto.request.UserUpdateRequestDto;
@@ -22,25 +23,22 @@ public class UserService {
 
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    // 회원가입
-    public UserSignupResponseDto signup(UserSignupRequestDto userReqDto ){
-
-        // 이메일이 존재하는 지 확인
-        if(userRepo.findByEmail(userReqDto.getEmail()).isPresent()) throw new DuplicateMemberException();
+    public UserSignupResponseDto signup(UserSignupRequestDto userReqDto) {
+        if (userRepo.findByEmail(userReqDto.getEmail()).isPresent()) {
+            throw new DuplicateMemberException();
+        }
 
         UserEntity saveEntity = userReqDto.toEntity();
 
-        // 프로필 이미지 처리
-        String fileName = fileSvc.profileUpload( userReqDto.getProfileUploadFile() );
-        if( fileName != null ) {
-            saveEntity.setProfileImageUrl( "/upload/profile/" + fileName );
+        String fileName = fileSvc.profileUpload(userReqDto.getProfileUploadFile());
+        if (fileName != null) {
+            saveEntity.setProfileImageUrl("/upload/profile/" + fileName);
         }
 
-        // 비밀번호 처리
-        String pwd = passwordEncoder.encode( userReqDto.getPassword() );
+        String pwd = passwordEncoder.encode(userReqDto.getPassword());
         saveEntity.setPassword(pwd);
 
-        try{
+        try {
             UserEntity savedEntity = userRepo.save(saveEntity);
             return UserSignupResponseDto.builder()
                     .email(savedEntity.getEmail())
@@ -48,27 +46,23 @@ public class UserService {
                     .profileImageUrl(savedEntity.getProfileImageUrl())
                     .createdAt(savedEntity.getCreatedAt())
                     .build();
-
-            // DB 유니크 제약 조건에 걸릴 시 --> 동시에 가입한 경우
-        } catch( DataIntegrityViolationException e){
+        } catch (DataIntegrityViolationException e) {
             fileSvc.deleteProfileFile(saveEntity.getProfileImageUrl());
             throw new DuplicateMemberException();
         }
     }
 
-    // 정보 조회
-    public UserInfoResponseDto getMyInfo(String email){
-        UserEntity userEntity = userRepo.findByEmail(email).orElse(null);
+    public UserInfoResponseDto getMyInfo(String email) {
+        UserEntity userEntity = userRepo.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
 
-        if(userEntity == null) return null;
         return userEntity.toInfoDto();
     }
 
-    // 정보수정
     @Transactional
     public UserInfoResponseDto updateUserInfo(String email, UserUpdateRequestDto userUpdDto) {
-        UserEntity userEntity = userRepo.findByEmail( email ).orElse(null);
-        if (userEntity == null) return null;
+        UserEntity userEntity = userRepo.findByEmail(email)
+                .orElseThrow(UserNotFoundException::new);
 
         if (userUpdDto.getNickname() != null) {
             userEntity.setNickname(userUpdDto.getNickname());
@@ -83,5 +77,4 @@ public class UserService {
 
         return userEntity.toInfoDto();
     }
-
 }
