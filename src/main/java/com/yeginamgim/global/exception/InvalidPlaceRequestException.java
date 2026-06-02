29b045858a2b637e1bef597734b08fe7b74cd3a9
bm1611 <bm1611@naver.com>
2026-06-02 11:56:57 +0900

    장소 조회 요청값 오류 전용 예외

---
+++ src/main/java/com/yeginamgim/global/exception/InvalidPlaceRequestException.java
@@ -0,0 +1,8 @@
+package com.yeginamgim.global.exception;
+
+public class InvalidPlaceRequestException extends RuntimeException {
+
+    public InvalidPlaceRequestException(String message) {
+        super(message);
+    }
+}
