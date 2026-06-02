48796c810e61259db86ba6a0f371377e03a7dee8
bm1611 <bm1611@naver.com>
2026-06-02 12:02:50 +0900

    로그인 응답 Dto 생성 책임

--- src/main/java/com/yeginamgim/auth/dto/response/LoginResponseDto.java
+++ src/main/java/com/yeginamgim/auth/dto/response/LoginResponseDto.java
@@ -1,5 +1,6 @@
 package com.yeginamgim.auth.dto.response;
 
+import com.yeginamgim.user.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,4 +15,14 @@ public class LoginResponseDto {
     private String email;
     private String nickname;
     private String profileImageUrl;
+
+    // 로그인한 사용자 정보와 발급된 JWT를 로그인 응답 DTO로 변환한다.
+    public static LoginResponseDto from(UserEntity userEntity, String token) {
+        return LoginResponseDto.builder()
+                .token(token)
+                .email(userEntity.getEmail())
+                .nickname(userEntity.getNickname())
+                .profileImageUrl(userEntity.getProfileImageUrl())
+                .build();
+    }
 }
