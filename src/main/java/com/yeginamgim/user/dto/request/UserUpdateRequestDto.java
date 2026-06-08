17aeb61c710b50b444ea5f5944e0a239e91a90b0
bm1611 <bm1611@naver.com>
2026-06-08 11:01:42 +0900

    nickname에 null허용, blank 거부 검증 추가

--- src/main/java/com/yeginamgim/user/dto/request/UserUpdateRequestDto.java
+++ src/main/java/com/yeginamgim/user/dto/request/UserUpdateRequestDto.java
@@ -1,7 +1,6 @@
 package com.yeginamgim.user.dto.request;
 
-import com.yeginamgim.user.entity.UserEntity;
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,27 +14,12 @@ import org.springframework.web.multipart.MultipartFile;
 @NoArgsConstructor
 // 회원 정보수정 요청에 대한 Dto
 public class UserUpdateRequestDto {
-    @Email(message = "email must be valid.")
-    @Size(max = 255, message = "email must be 255 characters or less.")
-    private String email;
-
+    @Pattern(regexp = ".*\\S.*", message = "nickname must not be blank.")
     @Size(max = 255, message = "nickname must be 255 characters or less.")
     private String nickname;
-
-    @Size(max = 1000, message = "profileImageUrl must be 1000 characters or less.")
-    private String profileImageUrl;
 
     @Size(max = 6, message = "birthDate must be 6 characters or less.")
     private String birthDate;
 
     private MultipartFile profileUploadFile;
-    private String updateAt;
-
-    public UserEntity toEntity(){
-        return UserEntity.builder()
-                .email( email )
-                .nickname( nickname )
-                .birthDate( birthDate )
-                .build();
-    }
 }
