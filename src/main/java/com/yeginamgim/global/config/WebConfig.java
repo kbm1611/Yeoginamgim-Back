0ea5d050bdb9448120d461acdd825e6951082847
lth0330 <dlxogud555@gmail.com>
2026-06-09 18:24:56 +0900

    배포 7

--- src/main/java/com/yeginamgim/global/config/WebConfig.java
+++ src/main/java/com/yeginamgim/global/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
                         CLOUDFRONT_FRONTEND_URL,
                         "http://localhost:*",
                         "http://127.0.0.1:*",
-                        "http://elasticbeanstalk-ap-northeast-2-988477084982.s3-website.ap-northeast-2.amazonaws.com/home"
+                        "http://elasticbeanstalk-ap-northeast-2-988477084982.s3-website.ap-northeast-2.amazonaws.com:*"
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
